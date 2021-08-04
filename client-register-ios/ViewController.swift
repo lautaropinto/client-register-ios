eb32fb06957e98bb44a2e1583cb0615fa89fc6c4
Lautaro Pinto <pintolautaro@outlook.com>
2021-08-04 19:06:22 -0300

    Initial commit.

---
+++ client-register-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-register-ios
+//
+//  Created by Lautaro Pinto on 04/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ client-register-iosTests/client_register_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  client_register_iosTests.swift
+//  client-register-iosTests
+//
+//  Created by Lautaro Pinto on 04/08/2021.
+//
+
+import XCTest
+@testable import client_register_ios
+
+class client_register_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
